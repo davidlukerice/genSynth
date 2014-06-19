87f7fa0850f0b5e88a616dde293442d9465d54c5
David Rice <davidlukerice@gmail.com>
2014-06-19 01:00:57 -0500

    updated ember models

--- app/app/models/instrument.js
+++ app/app/models/instrument.js
@@ -1,6 +1,10 @@
 
 export default DS.Model.extend({
-  userId: DS.attr('string'),
+  user: DS.belongsTo('user'),
   json: DS.attr('string'),
-  created: DS.attr('date')
+  created: DS.attr('date'),
+  branchedParent: DS.belongsTo('instrument'),
+  isPrivate: DS.attr('bool'),
+  likes: DS.hasMany('user'),
+  tags: DS.attr('string')
 });
