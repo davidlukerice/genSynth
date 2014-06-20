a422a19da253952f607cf79e127049c9135315f4
David Rice <davidlukerice@gmail.com>
2014-06-19 23:51:52 -0500

    fixed instrument likes relationship

--- app/app/models/instrument.js
+++ app/app/models/instrument.js
@@ -1,10 +1,14 @@
 
 export default DS.Model.extend({
-  user: DS.belongsTo('user'),
+  user: DS.belongsTo('user', {
+    inverse: 'instruments'
+  }),
   json: DS.attr('string'),
   created: DS.attr('date'),
   branchedParent: DS.belongsTo('instrument'),
-  isPrivate: DS.attr('bool'),
-  likes: DS.hasMany('user'),
+  isPrivate: DS.attr('boolean'),
+  likes: DS.hasMany('user', {
+    inverse: 'likes'
+  }),
   tags: DS.attr('string')
 });
