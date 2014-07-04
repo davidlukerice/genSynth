f3382e98b6538d992cdc88dda9a7a80577a2e92f
David Rice <davidlukerice@gmail.com>
2014-07-04 14:12:35 -0500

    starting to require auth to create an instrument

--- server/server/routes/instruments.js
+++ server/server/routes/instruments.js
@@ -3,7 +3,9 @@ module.exports = function(app, passport, auth) {
   var instruments = require('../controllers/instruments');
   app.get('/instruments', instruments.index);
   // app.post('/instruments', auth.requiresLogin, instruments.create);
-  app.post('/instruments', instruments.create);
+  
+  app.post('/instruments', auth.requiresLogin, auth.instrument.hasAuthorization, instruments.create);
+  
   app.get('/instruments/:instrumentId', instruments.show);
   // app.put('/instruments/:instrumentId', auth.requiresLogin, auth.instrument.hasAuthorization, instruments.update);
   // app.del('/instruments/:instrumentId', auth.requiresLogin, auth.instrument.hasAuthorization, instruments.destroy);
