18f90ceea2dba41509d453dd6292f873ff4e773e
davidlukerice <davidlukerice@gmail.com>
2014-07-20 17:36:44 -0500

    show user's correct username on user page

--- app/app/routes/user.js
+++ app/app/routes/user.js
@@ -4,6 +4,7 @@ export default Ember.Route.extend({
 
   model: function(params) {
     return Ember.Object.create({
+      user: this.store.find('user', params.user_id),
       instruments: this.store.find('instrument', {user: params.user_id})
     });
   },
