5a05517d24e828dfab9400403e37bec19d7c6b90
davidlukerice <davidlukerice@gmail.com>
2014-08-11 11:00:12 -0500

    fix entering user page not loading correct model

--- app/app/routes/user.js
+++ app/app/routes/user.js
@@ -3,14 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   model: function(params) {
-    return Ember.Object.create({
-      user: this.store.find('user', params.user_id),
-      instruments: this.store.find('instrument', {user: params.user_id})
-    });
+    return this.store.find('user', params.user_id);
   },
 
   setupController: function(controller, model) {
-    controller.set('user', model.get('user'));
-    controller.set('instruments', model.get('instruments'));
+    controller.set('user', model);
+    controller.set('instruments', this.store.find('instrument', {
+      user: model.id
+    }));
   }
 });
