4ede498017919f051583d98de6d0708624058b92
davidlukerice <davidlukerice@gmail.com>
2014-08-16 22:46:48 -0500

    make dbpath default to d:\

--- server/db.bat
+++ server/db.bat
@@ -1,2 +1,2 @@
 mkdir c:\mongodb\genSynth
-"C:\Program Files\MongoDB 2.6 Standard\bin\mongod.exe" --dbpath c:\mongodb\genSynth
+"C:\Program Files\MongoDB 2.6 Standard\bin\mongod.exe" --dbpath d:\mongodb\genSynth
