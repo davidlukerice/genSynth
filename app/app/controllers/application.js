46caeb7ec6e6c58c0a310392669080fa88d585cd
davidlukerice <davidlukerice@gmail.com>
2014-08-28 22:53:20 -0500

    splitting app auth work into a mixin

--- app/app/controllers/application.js
+++ app/app/controllers/application.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 import InstrumentPlayer from 'gen-synth/mixins/instrument-player';
+import AuthHandler from 'gen-synth/mixins/auth-handler';
 
-export default Ember.Controller.extend(InstrumentPlayer, {
+export default Ember.Controller.extend(InstrumentPlayer, AuthHandler, {
   evolvePageIsActive: function() {
     return this.get('currentPath') === 'evolve';
   }.property('currentPath'),
@@ -20,53 +21,10 @@ export default Ember.Controller.extend(InstrumentPlayer, {
 
   currentUser: null,
 
-  showingLogin: false,
-
   showSettings: false,
   showHelp: false,
 
-  loadCurrentUser: function() {
-    if (!this.get('session.isAuthenticated'))
-      return;
-
-    var self = this,
-        sessionContent = this.get('session.content'),
-        accessToken = sessionContent.accessToken;
-
-    // TODO: Get actual token when reloading page
-
-    //var provider = sessionContent.provider;
-    //var userId = sessionContent.userId;
-    console.log('content: '+JSON.stringify(sessionContent));
-    Ember.$.ajax({
-      url: 'http://localhost:3000/auth/facebook',
-      type: 'GET',
-      data: {
-        access_token: accessToken
-      },
-      //crossDomain: true,
-      xhrFields: {
-        withCredentials: true
-      }
-    }).then(function(response) {
-      console.log('loginResponse: '+JSON.stringify(response));
-      self.store.find('user', response.user).then(function(user) {
-        self.set('currentUser', user);
-      });
-    }, function(xhr, status, error) {
-      console.log('error: '+error.message);
-      self.send('invalidateSession');
-    });
-  }.observes('session.isAuthenticated').on('init'),
-
   actions: {
-    showLogin: function() {
-      this.set('showLogin', true);
-    },
-    hideLogin: function() {
-      this.set('showLogin', false);
-    },
-
     toggleHelp: function() {
       this.set('showHelp', !this.get('showHelp'));
     },
---
+++ app/app/mixins/auth-handler.js
@@ -0,0 +1,52 @@
+import Ember from 'ember';
+
+// Can hold a current midi input
+export default Ember.Mixin.create({
+  showingLogin: false,
+
+  username: "",
+  password: "",
+
+  loadCurrentUser: function() {
+    if (!this.get('session.isAuthenticated'))
+      return;
+
+    var self = this,
+        sessionContent = this.get('session.content'),
+        accessToken = sessionContent.accessToken;
+
+    // TODO: Get actual token when reloading page
+
+    //var provider = sessionContent.provider;
+    //var userId = sessionContent.userId;
+    console.log('content: '+JSON.stringify(sessionContent));
+    Ember.$.ajax({
+      url: 'http://localhost:3000/auth/facebook',
+      type: 'GET',
+      data: {
+        access_token: accessToken
+      },
+      //crossDomain: true,
+      xhrFields: {
+        withCredentials: true
+      }
+    }).then(function(response) {
+      console.log('loginResponse: '+JSON.stringify(response));
+      self.store.find('user', response.user).then(function(user) {
+        self.set('currentUser', user);
+      });
+    }, function(xhr, status, error) {
+      console.log('error: '+error.message);
+      self.send('invalidateSession');
+    });
+  }.observes('session.isAuthenticated').on('init'),
+
+  actions: {
+    showLogin: function() {
+      this.set('showLogin', true);
+    },
+    hideLogin: function() {
+      this.set('showLogin', false);
+    }
+  }
+});
