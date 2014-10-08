ae542eefebaa893635570ebd32dfe9c06dbd62b7
davidlukerice <davidlukerice@gmail.com>
2014-10-07 22:37:51 -0500

    strip ids (and refresh them on each website) to prep for making the repo public

--- app/config/auth-providers.js
+++ app/config/auth-providers.js
@@ -1,10 +1,6 @@
 module.exports = {
   crossOriginWhitelist: ['http://localhost:3000'],
   facebookConnect: {
-    appId: '246499025545134'
-  },
-  facebookOAuth2: {
-    apiKey:      '246499025545134',
-    redirectUri: 'http://localhost:4200'
+    appId: 'XXXXXXXXXXXXXXXX'
   }
 };
