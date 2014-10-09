fece65568486637a7e9758cda30f764a7ead2970
davidlukerice <davidlukerice@gmail.com>
2014-10-08 22:18:54 -0500

    fix '' csp issue

--- app/config/environment.js
+++ app/config/environment.js
@@ -14,8 +14,8 @@ module.exports = function(environment) {
       }
     },
     contentSecurityPolicy: {
-      'default-src': "'none'",
       'script-src': "'self' 'unsafe-inline' 'unsafe-eval' connect.facebook.net",
+      'object-src': "'self'",
       'font-src': "'self' data: fonts.gstatic.com",
       'connect-src': "'self' localhost:3000",
       //'img-src': "'self' www.facebook.com",
