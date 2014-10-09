cd9791ae0fe77f768b7850bb0577f84db2aea302
davidlukerice <davidlukerice@gmail.com>
2014-10-08 21:36:05 -0500

    fix update express server to v4

--- server/server/config/express.js
+++ server/server/config/express.js
@@ -1,16 +1,21 @@
 /**
  * Module dependencies.
  */
-var express = require('express'),
-  mongoStore = require('connect-mongo')(express),
-  helpers = require('view-helpers'),
-  config = require('./config');
+var session = require('express-session'),
+    compression = require('compression'),
+    morgan = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    mongoStore = require('connect-mongo')(session),
+    helpers = require('view-helpers'),
+    config = require('./config');
 
 module.exports = function(app, passport, db) {
-  app.set('showStackError', true);    
+  app.set('showStackError', true);
 
   //Should be placed before express.static
-  app.use(express.compress({
+  app.use(compression({
     filter: function(req, res) {
       return (/json|text|javascript|css/).test(res.getHeader('Content-Type'));
     },
@@ -19,7 +24,7 @@ module.exports = function(app, passport, db) {
 
   //Don't use logger for test env
   if (process.env.NODE_ENV !== 'test') {
-    app.use(express.logger('dev'));
+    app.use(morgan('dev'));
   }
 
   //Set views path, template engine and default layout
@@ -30,53 +35,34 @@ module.exports = function(app, passport, db) {
   //Enable jsonp
   app.enable("jsonp callback");
 
-  app.configure(function() {
-    //cookieParser should be above session
-    app.use(express.cookieParser());
+  //cookieParser should be above session
+  app.use(cookieParser());
 
-    app.use(function(req, res, next) {
-      res.setHeader('Access-Control-Allow-Origin', config.accessControlAllowOrigin);
-      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-      res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-      res.setHeader('Access-Control-Allow-Credentials', true);
-      next();
-    });
-
-    //bodyParser should be above methodOverride
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-
-    //express/mongo session storage
-    app.use(express.session({
-      secret: config.session,
-      store: new mongoStore({
-        db: db.connection.db,
-        collection: 'sessions'
-      })
-    }));
-
-    //dynamic helpers
-    app.use(helpers(config.app.name));
-
-    //use passport session
-    app.use(passport.initialize());
-    app.use(passport.session());
-    
-    //routes should be at the last
-    app.use(app.router);
-
-    //Assume "not found" in the error msgs is a 404. this is somewhat silly, but valid, you can do whatever you like, set properties, use instanceof etc.
-    //app.use(function(err, req, res, next) {
-    //  //Treat as 404
-    //  if (~err.message.indexOf('not found')) return next();
-//
-    //  //Log it
-    //  console.error(err.stack);
-//
-    //  //Error page
-    //  res.status(500).render('500', {
-    //    error: err.stack
-    //  });
-    //});
+  app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', config.accessControlAllowOrigin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
   });
+
+  //bodyParser should be above methodOverride
+  app.use(bodyParser());
+  app.use(methodOverride());
+
+  //express/mongo session storage
+  app.use(session({
+    secret: config.session,
+    store: new mongoStore({
+      db: db.connection.db,
+      collection: 'sessions'
+    })
+  }));
+
+  //dynamic helpers
+  app.use(helpers(config.app.name));
+
+  //use passport session
+  app.use(passport.initialize());
+  app.use(passport.session());
 };
